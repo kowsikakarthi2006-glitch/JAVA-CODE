bbb491721a17055a937228ec1bd13123622397a6
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 21:16:46 +0530

    Create Narrowcasting.java

---
+++ Narrowcasting.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args)
+    {
+       double CGPA=8.05;
+       int a=(int)CGPA;
+        System.out.println(CGPA);
+         System.out.println();
+    }
+}
