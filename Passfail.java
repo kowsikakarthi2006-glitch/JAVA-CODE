d9ee40c8a503dcd0124c2f8e4748b16e764aa6a5
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 21:20:28 +0530

    Create Passfail.java

---
+++ Passfail.java
@@ -0,0 +1,12 @@
+class main{
+    public static void main(String[] args) {
+        int marks= 45;
+        if(marks>=50){
+        System.out.println("pass");
+        } else {
+            System.out.println("Fail");
+        }
+        
+        }
+
+}
