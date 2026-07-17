98df5f4daf9f691972c78539c6bf646be98f1857
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 14:49:03 +0530

    Update SimpleCalculator.java

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,1 +1,14 @@
-
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        int a=sc.nextInt();
+        System.out.print("Enter the second number:");
+        int b=sc.nextInt();
+        System.out.println("Addition="+(a+b));
+         System.out.println("Substraction="+(a-b));
+          System.out.println("Multiplication="+(a*b));
+           System.out.println("Division="+(a/b));
+    }
+}
