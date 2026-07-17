05ac16ed0ee54b3b2e1f4018dcab4aa257d88594
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 21:26:26 +0530

    Create Usingbooleanprogram.java

---
+++ Usingbooleanprogram.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        boolean hasID=true;
+        if (age>=18 && hasID){
+        System.out.println("Entry Allowed");
+    }
+}
+}
