7ad7dfb5af0b2d3d414b411cb5e8433107486206
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 21:41:44 +0530

    Create Leapyearcountusingfor.java

---
+++ Leapyearcountusingfor.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        int count =0;
+        for (int i=2005;i<=2026;i++){
+            if (i%4==0)
+            count++;
+        }
+        System.out.println(count);
+    }
+}
