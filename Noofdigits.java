e1e0da61f3a717315a5babc1a99cf7475a9dcaef
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 21:51:14 +0530

    Create Noofdigits.java

---
+++ Noofdigits.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        int count=0;
+        while (n>0)
+        {
+            count=count+1;
+            n=n/10;
+        }
+        System.out.println("no.of digits:"+count);
+    }
+}
