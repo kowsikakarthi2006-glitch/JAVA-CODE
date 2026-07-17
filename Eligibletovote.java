53bb9eb672f65826fb29720683cc2c7d683b3e76
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 21:18:36 +0530

    Create Eligibletovote.java

---
+++ Eligibletovote.java
@@ -0,0 +1,9 @@
+class main{
+    public static void main(String[] args) {
+        int age= 20;
+        if(age>=18){
+        System.out.println("Your are eligible to vote");
+        }
+    }
+}
+
