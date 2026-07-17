ee033aaae07af71de8722207a773960cf4be20b1
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 21:25:15 +0530

    Create Temoprogram.java

---
+++ Temoprogram.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        int temp=35;
+        if (temp>40){
+        System.out.println("Very Hot");
+    }
+    else if (temp>30){
+        System.out.println("Hot");
+    }
+    else if (temp>20){
+        System.out.println("Warm");
+    }
+    else {
+        System.out.println("Cold");
+    }
+}
+}
