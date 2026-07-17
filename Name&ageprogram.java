53b11190dd153e028f0233cd5c10f6c6a7fc73fb
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 20:57:36 +0530

    Create Name&ageprogram.java

---
+++ Name&ageprogram.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+ Scanner sc=new Scanner(System.in);
+ System.out.print("Enter the name:");
+ String name=sc.nextLine();
+ System.out.print("Enter the Age:");
+ int a=sc.nextInt();
+ System.out.println("Name is"+name);
+  System.out.println("Age is"+a);
+    }
+}
