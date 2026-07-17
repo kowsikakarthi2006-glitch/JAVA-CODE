e8d0f4e9fc1052504f23f545507fb46d828a93e0
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 21:08:58 +0530

    Create Averageprogram.java

---
+++ Averageprogram.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+int Tamil=88;
+int English=90;
+int Maths=70;
+int Science=60;
+int Social=89;
+System.out.println((Tamil+English+Maths+Science+Social)/5);
+    }
+}
