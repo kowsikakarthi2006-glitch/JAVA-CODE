dc0e593e3faf516b3065dfcf05bdfe2334b4c5ae
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 21:14:06 +0530

    Create Integertypes.java

---
+++ Integertypes.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args)
+    {
+        byte a= 99;
+        short b=20000;
+        int c=480000;
+        System.out.println("Mark"+a);
+         System.out.println("Monthly Salary"+b);
+          System.out.println("Annual Income"+c);
+    }
+}
