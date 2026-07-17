efb4033c8a34d2452daf1b88a481217172a88de2
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 21:39:49 +0530

    Create Leapyearcount.java

---
+++ Leapyearcount.java
@@ -0,0 +1,16 @@
+public class main{
+    public static void main(String[]args) {
+    int i = 2005;
+    int count=0;
+    while(i<=2026){
+        if(i%4==0){
+            count ++;
+        i++;
+    } 
+    else{
+        i++;}
+  }
+  System.out.println(count);
+}
+}
+
