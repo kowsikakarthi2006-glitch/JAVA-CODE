3ce2aaa060cca09af4a435f8d46cd8e2b1993a24
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 21:27:28 +0530

    Create Usingbooleanexample2.java

---
+++ Usingbooleanexample2.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int marks = 95;
+        boolean SportsQuota= true;
+        if (marks>=90||SportsQuota){
+        System.out.println("Admission Granted");
+    }
+}
+}
