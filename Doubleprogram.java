0def12f8680cf85048a95bcc5b61d8288f4d9c68
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 21:15:20 +0530

    Create Doubleprogram.java

---
+++ Doubleprogram.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args)
+    {
+        int a=9;
+        double b=(double)a;
+        System.out.println(a);
+         System.out.println(b);
+    }
+}
