6fcc00f425378918e1d7556906783029bc1df475
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 20:53:49 +0530

    Create Variables&DataTypes.java

---
+++ Variables&DataTypes.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args)
+    {
+        String Str = "kowsika";
+        int Tamil = 88;
+        int English = 90;
+        int Maths = 70;
+        int Science = 60;
+        int Social = 89;
+        System.out.println("Name="+Str);
+        System.out.println("Tamil="+Tamil);
+        System.out.println("English="+English);
+        System.out.println("Maths="+Maths);
+        System.out.println("Science="+Science);
+        System.out.println("Social="+Social);
+    }
+}
