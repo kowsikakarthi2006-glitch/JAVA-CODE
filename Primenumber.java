efd75cb4cc3f9866d80d2fbd0e8fb36d6ebced01
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-17 19:52:21 +0530

    Create Primenumber.java

---
+++ Primenumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int n=13;
+        String str = prime(n);
+        System.out.println(str);
+    }
+    static String prime(int k)
+    {
+        if(k<2)
+        { return " not prime";
+    }
+    for (int i=2;i<k;i++){
+        if (k%i==0)
+        return "not prime";
+    }
+    return "prime";
+}
+}
