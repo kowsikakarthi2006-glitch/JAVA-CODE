539a30050a9ffc5693d72bc454abe296858d0daf
kowsikakarthi2006-glitch <kowsikakarthi2006@gmail.com>
2026-07-18 15:37:08 +0530

    Update Usingscannersubjectmark.java

--- Usingscannersubjectmark.java
+++ Usingscannersubjectmark.java
@@ -1,25 +1,30 @@
 import java.util.*;
+
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int tamil = 88;
-        int english = 90;
-        int maths = 70;
-        int science = 60;
-        int social = 89;
+
         System.out.print("Enter the subject name: ");
         String subject = sc.next();
-        if(subject.equalsIgnoreCase("Tamil"))
-            System.out.println("Marks = " + tamil);
-        else if(subject.equalsIgnoreCase("English"))
-            System.out.println("Marks = " + english);
-        else if(subject.equalsIgnoreCase("Maths"))
-            System.out.println("Marks = " + maths);
-        else if(subject.equalsIgnoreCase("Science"))
-            System.out.println("Marks = " + science);
-        else if(subject.equalsIgnoreCase("Social"))
-            System.out.println("Marks = " + social);
-        else
-            System.out.println("Invalid Subject");
+
+        switch(subject) {
+            case "Tamil":
+                System.out.println("Marks = 88");
+                break;
+            case "English":
+                System.out.println("Marks = 90");
+                break;
+            case "Maths":
+                System.out.println("Marks = 70");
+                break;
+            case "Science":
+                System.out.println("Marks = 60");
+                break;
+            case "Social":
+                System.out.println("Marks = 89");
+                break;
+            default:
+                System.out.println("Invalid Subject");
+        }
     }
 }
