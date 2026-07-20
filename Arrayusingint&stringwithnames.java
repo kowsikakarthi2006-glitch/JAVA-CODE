9017edc9c7f53390c9eede90447007124a65ba0b
Kowsika Karthikeyan <kowsikakarthi2006@gmail.com>
2026-07-20 14:48:16 +0530

    Create Arrayusingint&stringwithnames.java

---
+++ Arrayusingint&stringwithnames.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+      int [] marks={420,410,450,460,430,424,482,410,432,468};
+      String[]name={"DEEPIKA","MONI","KAVIYA","KOWSI","VAISU","ABI","REVATHI","MALATHI","ANUSIYA","NANTHINI"};
+      for(int i=0;i<marks.length;i++)
+      {
+          System.out.println(name[i]+"="+marks[i]);
+      }
+    }
+}
