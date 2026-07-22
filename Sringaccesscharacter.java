f2f38f411a3a5395259d68113fb06b196f78272c
Kowsika Karthikeyan <kowsikakarthi2006@gmail.com>
2026-07-22 06:11:25 +0530

    Create Sringaccesscharacter.java

---
+++ Sringaccesscharacter.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        String name="JAVA";
+        System.out.println(name.charAt(1));
+        System.out.println(name.charAt(2));
+    }
+}
