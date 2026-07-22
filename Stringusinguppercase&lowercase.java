435a7773b6c1b3bc4388f3e6a4178fc845c646d8
Kowsika Karthikeyan <kowsikakarthi2006@gmail.com>
2026-07-22 06:23:23 +0530

    Create Stringusinguppercase&lowercase.java

---
+++ Stringusinguppercase&lowercase.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        String name = "KoWsIka";
+
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+    }
+}
