80b6c1628f39d0be45932657d9c0268a576d8561
Kowsika Karthikeyan <kowsikakarthi2006@gmail.com>
2026-07-22 06:07:26 +0530

    Create Transporse.java

---
+++ Transporse.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+       int [][]arr={
+           {1,2,3},
+            {4,5,6}
+    };
+    System.out.println("Transporse Matrix:");
+    for (int i=0;i<arr[0].length;i++)
+    {
+        for(int j=0;j<arr.length;j++)
+        {
+            System.out.print(arr[j][i]+" ");
+        }
+        System.out.println();
+    }
+}
+}
