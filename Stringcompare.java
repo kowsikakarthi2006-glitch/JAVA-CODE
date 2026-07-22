382907cf7f27543f797a52b0a318f46a532e126d
Kowsika Karthikeyan <kowsikakarthi2006@gmail.com>
2026-07-22 06:26:43 +0530

    Create Stringcompare.java

---
+++ Stringcompare.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        String str1 = "KOWSIKAKARTHIKEYAN";
+        String str2 = "KOWSIKAKARTHIKEYAN";
+        System.out.println(str1.equals(str2));
+    }
+}
