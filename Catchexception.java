aeebfffdd62d0ff90587d5c793b3d4b09500eb56
Kowsika Karthikeyan <kowsikakarthi2006@gmail.com>
2026-07-28 14:20:18 +0530

    Create Catchexception.java

---
+++ Catchexception.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            int result = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero.");
+        }
+
+    }
+}
